8cab9f6bcabd3e66e6cc08c168075a05e3eac755
Jiaming Hu <horis233@gmail.com>
2022-07-12 23:21:50 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,7 +37,7 @@ module.exports = {
       resolve: `gatsby-plugin-gdpr-cookies`,
       options: {
         facebookPixel: {
-          pixelId: "368538235370513",
+          pixelId: "612968713333445",
           cookieName: "cookie-consent",
         },
         environments: ["production", "development"],
